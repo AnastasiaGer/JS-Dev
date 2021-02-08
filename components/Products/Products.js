644d69d8b91a0b3b6f7975ef84c1f2ae8c34b6c9
AnastasiaGer <aagermanova@gmail.com>
2021-02-08 11:28:57 -0800

    LocalStorageUtil

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,27 +1,28 @@
 class Products {
 
-
-
   render() {
-    let htmlCatalog = ' ';
-    CATALOG.forEach(({id, name, price, img}) => {
-      htmlCatalog += `
-      <li>
-        <span>${name}</span>
-        <img src="${img}">
-        <span>${price}</span>
-        <button>Add to basket</button>
-      </li>
-      `
-    });
+      let htmlCatalog = '';
 
-    const html = `
-    <ul>
-      ${htmlCatalog}
-    </ul>
-    `;
+      CATALOG.forEach(({ id, name, price, img }) => {
+          htmlCatalog += `
+              <li class="products-element">
+                  <span class="products-element__name">${name}</span>
+                  <img class="products-element__img" src="${img}" />
+                  <span class="products-element__price">
+                  ⚡️ ${price.toLocaleString()} USD
+                  </span>
+                  <button class="products-element__btn">Добавить в корзину</button>
+              </li>
+          `;
+      });
 
-    ROOT_PRODUCTS.innerHTML = html;
+      const html = `
+          <ul class="products-container">
+              ${htmlCatalog}
+          </ul>
+      `;
+
+      ROOT_PRODUCTS.innerHTML = html;
   }
 }
 
