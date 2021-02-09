80de05b2b5142b301c65ecaf7951a7686c2e60e2
AnastasiaGer <aagermanova@gmail.com>
2021-02-09 11:33:32 -0800

    shopping

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,19 +1,22 @@
 class Header {
-
+  handlerOpenShoppingPage() {
+      shoppingPage.render();
+  }
 
   render(count) {
-    const html = `
-      <div class="header-container"> 
-        <div class="header-counter">  🔥 ${count} </div>
-      </div>  
-    `;
+      const html = `
+         <div class="header-container">
+              <div class="header-counter" onclick="headerPage.handlerOpenShoppingPage();">
+                  🔥 ${count}
+              </div>
+         </div>
+      `;
 
-    ROOT_HEADER.innerHTML = html;
+      ROOT_HEADER.innerHTML = html;
   }
 }
 
 const headerPage = new Header();
 
-const productStore = localStorageUtil.getProducts();
-
-headerPage.render(productStore.length);
+const productsStore = localStorageUtil.getProducts();
+headerPage.render(productsStore.length);
--- components/Products/Products.js
+++ components/Products/Products.js
@@ -15,7 +15,8 @@ class Products {
             element.classList.remove(this.classNameActive);
             element.innerHTML = this.labelAdd;
         }
-        headerPage.render(products.length)
+
+        headerPage.render(products.length);
     }
 
     render() {
