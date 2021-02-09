bb5f00cc36855e5f94822e796e75bde84ce5bf08
AnastasiaGer <aagermanova@gmail.com>
2021-02-09 10:59:09 -0800

    products button active

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,29 +1,59 @@
 class Products {
+    constructor() {
+        this.classNameActive = 'products-element__btn_active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
 
-  render() {
-      let htmlCatalog = '';
+    handleSetLocationStorage(element, id) {
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+        
+        if (pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+    }
 
-      CATALOG.forEach(({ id, name, price, img }) => {
-          htmlCatalog += `
-              <li class="products-element">
-                  <span class="products-element__name">${name}</span>
-                  <img class="products-element__img" src="${img}" />
-                  <span class="products-element__price">
-                  ⚡️ ${price.toLocaleString()} USD
-                  </span>
-                  <button class="products-element__btn">Добавить в корзину</button>
-              </li>
-          `;
-      });
+    render() {
+        const productsStore = localStorageUtil.getProducts();
+        let htmlCatalog = '';
 
-      const html = `
-          <ul class="products-container">
-              ${htmlCatalog}
-          </ul>
-      `;
+        CATALOG.forEach(({ id, name, price, img }) => {
+            let activeClass = '';
+            let activeText = '';
 
-      ROOT_PRODUCTS.innerHTML = html;
-  }
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd;
+            } else {
+                activeClass = ' '+this.classNameActive;
+                activeText = this.labelRemove;
+            }
+
+            htmlCatalog += `
+                <li class="products-element">
+                    <span class="products-element__name">${name}</span>
+                    <img class="products-element__img" src="${img}" />
+                    <span class="products-element__price">
+                        ⚡️ ${price.toLocaleString()} USD
+                    </span>
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+                        ${activeText}
+                    </button>
+                </li>
+            `;
+        });
+
+        const html = `
+            <ul class="products-container">
+                ${htmlCatalog}
+            </ul>
+        `;
+
+        ROOT_PRODUCTS.innerHTML = html;
+    }
 }
 
 const productsPage = new Products();
--- utils/localstorageUtil.js
+++ utils/localstorageUtil.js
@@ -1,34 +1,31 @@
 class LocalStorageUtil {
   constructor() {
-    this.keyName = 'products';
+      this.keyName = 'products';
   }
 
   getProducts() {
-    const productsLocalStorage = localStorage.getItem(this.keyName);
-    if (productsLocalStorage !== null) {
-      return JSON.parse(productsLocalStorage)
-    } else {
+      const productsLocalStorage = localStorage.getItem(this.keyName);
+      if (productsLocalStorage !== null) {
+          return JSON.parse(productsLocalStorage);
+      }
       return [];
-    }
   }
 
   putProducts(id) {
-    let products = this.getProducts();
-    let pushProduct = false
-    const index = products.indexOf(id);
-    if(index === -1) {
-      products.push(id);
-      let pushProduct = true
-    } else {
-      products.splice(index, 1)
-    }
-    
-    localStorage.setItem(this.keyName, JSON.stringify(products))
+      let products = this.getProducts();
+      let pushProduct = false;
+      const index = products.indexOf(id);
 
-    return {
-      pushProduct: pushProduct,
-      products: products
-    }
+      if (index === -1) {
+          products.push(id);
+          pushProduct = true;
+      } else {
+          products.splice(index, 1);
+      }
+
+      localStorage.setItem(this.keyName, JSON.stringify(products));
+
+      return { pushProduct, products }
   }
 }
 
